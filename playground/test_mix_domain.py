a11f28271beea26706f0fcd2d680fe0656d106c9
Liu Huaqing <2088623278@qq.com>
2026-09-09 11:42:58 +0800

    refactor(domain):重构混合元生成空间为使用basix.ufl而不是scifem，避免对即将废弃项目的依赖

--- playground/test_mix_domain.py
+++ playground/test_mix_domain.py
@@ -1,14 +1,18 @@
 import ufl
 from dolfinx import mesh, fem
-from scifem import create_real_functionspace
+from basix.ufl import real_element
 from mpi4py import MPI
-
 
 domain = mesh.create_unit_interval(MPI.COMM_WORLD, 10)
 
+# 创建常规函数空间
 V_u = fem.functionspace(domain, ("Lagrange", 2))
 
-R = create_real_functionspace(domain)
+# 用 real_element 创建 Real 元素，再手动构造函数空间
+real_elem = real_element(domain.basix_cell())
+R = fem.functionspace(domain, real_elem)
+
+# 用 ufl.MixedFunctionSpace 组合两个空间
 V = ufl.MixedFunctionSpace(V_u, R)
 
 u, eps = ufl.TrialFunctions(V)
